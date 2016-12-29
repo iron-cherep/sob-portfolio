db175545681936864846c9447c69ae72eeca471c
Anton Cherepov <jacobxblue@gmail.com>
2016-12-29 18:04:02 +0300

    сверстал страницы проектов, исправил баги на мобильном, изменил версию jquery для совместимости

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
+  //добавляем задержку обработки тапов по карточке, чтобы избежать нажатия
+  //кнопки до того, как она появилась визуально
+  $(".page-cards__card-image").on("tap", function(event) {
+
+    var cardInfo = $(this).parent().next();
+    cardInfo.css("pointer-events", "none");
+
+    setTimeout(function() {
+        cardInfo.css("pointer-events", "all");
+    }, 300);
+  });
+
+  //инициализируем метод плагина scrollSpeed для ускоренного скролла на десктопе
   jQuery.scrollSpeed(200, 800);
 
 });
